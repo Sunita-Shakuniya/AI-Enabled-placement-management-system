30bfc17b168f42e4d79b8e2d850395d28ac58bc2
Sunita-Shakuniya <sunitashakuniya@gmail.com>
2025-04-04 10:41:49 +0530

    change react config

--- placementfrontend/vite.config.js
+++ placementfrontend/vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  /** 
   server:{
     proxy:{
       '/api':'http://127.0.0.1:4000/',//proxy work at development mode not for producation
     },
 
-  },
+  },*/
   plugins: [react()],
 })
