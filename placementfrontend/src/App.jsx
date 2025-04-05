eb624eca5633b03971fea0252a671d755e4bdff1
Sunita-Shakuniya <sunitashakuniya@gmail.com>
2025-04-05 13:42:14 +0530

    change BrowserRouter to HashRouter

--- placementfrontend/src/App.jsx
+++ placementfrontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about_us/About";
 import AdminDashboard from "./pages/admin/Dashboard.admin";
